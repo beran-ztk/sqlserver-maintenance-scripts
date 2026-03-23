1dd3e07d668d3981f4c7191a6afa5f9922d226b5
Beran Öztürk <b.oeztuerk@apotechnik.de>
2026-03-23 11:50:51 +0100

    verordnunglöschen aktualasiert

--- archieve/work/Verordnung.loeschen.sql
+++ archieve/work/Verordnung.loeschen.sql
@@ -2,14 +2,6 @@ WITH XMLNAMESPACES(DEFAULT 'http://hl7.org/fhir')
 SELECT a.ERezeptID, (a.blob.value('(//extension[@url=''https://gematik.de/fhir/erp/StructureDefinition/GEM_ERP_EX_AcceptDate'']/valueDate/@value)[1]', 'nvarchar(max)')) AS x
 FROM RW_EREZEPT_Verordnung a
 	INNER JOIN RW_EREZEPT_Verlorene v ON a.ERezeptID = v.ERezeptID
-WHERE (REPLACE(a.blob.value('(//extension[@url=''https://gematik.de/fhir/erp/StructureDefinition/GEM_ERP_EX_AcceptDate'']/valueDate/@value)[1]', 'nvarchar(max)'), '-', '')) < DATEADD(WEEK, -8, GETDATE())
+WHERE (REPLACE(a.blob.value('(//extension[@url=''https://gematik.de/fhir/erp/StructureDefinition/GEM_ERP_EX_AcceptDate'']/valueDate/@value)[1]', 'nvarchar(max)'), '-', '')) 
+		< DATEADD(DAY, -100, GETDATE()) -- Select, wo AcceptDate älter als 100 Tage
 ORDER BY x desc
-
-WITH XMLNAMESPACES(DEFAULT 'http://hl7.org/fhir')
-DELETE RW_EREZEPT_Verordnung WHERE ERezeptID IN
-(
-SELECT a.ERezeptID
-FROM RW_EREZEPT_Verordnung a
-	INNER JOIN RW_EREZEPT_Verlorene v ON a.ERezeptID = v.ERezeptID
-WHERE (REPLACE(a.blob.value('(//extension[@url=''https://gematik.de/fhir/erp/StructureDefinition/GEM_ERP_EX_AcceptDate'']/valueDate/@value)[1]', 'nvarchar(max)'), '-', '')) < DATEADD(WEEK, -8, GETDATE())
-);
